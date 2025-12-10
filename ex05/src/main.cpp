9d52013c1d9dabb234224f334335ca7295e59147
Angele Barzilai <abarzila@z1r10p4.42lyon.fr>
2025-12-10 09:52:35 +0100

    Harl main better

--- ex05/src/main.cpp
+++ ex05/src/main.cpp
@@ -1,15 +1,32 @@
 #include "Harl.hpp"
 
-int main(void)
+int main(int ac, char **av)
 {
     Harl harl;
-
-    harl.complain("DEBUG");
-    harl.complain("INFO");
-    harl.complain("RANDOM");
-    harl.complain("");
-    harl.complain("WARNING");
-    harl.complain("ERROR");
-
+    
+    if (ac <= 1)
+    return (0);
+    
+    int i = 1;
+    while (av[i])
+    {
+        harl.complain(av[i]);
+        i++;
+    }
+    
     return (0);
 }
+
+// int main(void)
+// {
+//     Harl harl;
+
+//     harl.complain("DEBUG");
+//     harl.complain("INFO");
+//     harl.complain("RANDOM");
+//     harl.complain("");
+//     harl.complain("WARNING");
+//     harl.complain("ERROR");
+
+//     return (0);
+// }
--- ex06/src/main.cpp
+++ ex06/src/main.cpp
@@ -12,3 +12,23 @@ int main(int ac, char **av)
     harl.complain(av[1]);
     return (0);
 }
+
+// int main(void)
+// {
+//     Harl harl;
+
+//     harl.complain("DEBUG");
+//     std::cout << "-" << std::endl;
+//     harl.complain("INFO");
+//     std::cout << "-" << std::endl;
+//     harl.complain("RANDOM");
+//     std::cout << "-" << std::endl;
+//     harl.complain("");
+//     std::cout << "-" << std::endl;
+//     harl.complain("WARNING");
+//     std::cout << "-" << std::endl;
+//     harl.complain("ERROR");
+//     std::cout << "-" << std::endl;
+
+//     return (0);
+// }
